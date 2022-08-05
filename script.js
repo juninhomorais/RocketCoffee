729a70d6897991f7d1315b9e5549507c18e83ef9
Júnior <juniorfrancademorais@gmail.com>
2022-08-05 18:59:25 -0300

    Correcting image address

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function alterar() {
     let tela = innerWidth
     if (tela <= 600) {
-        document.getElementById('logo').src = './assets/logo-mobile.svg'
+        document.getElementById('logo').src = './images/logo-mobile.svg'
     } else {
-        document.getElementById('logo').src = './assets/logo-desktop.svg'
+        document.getElementById('logo').src = './images/logo-desktop.svg'
     }
 }
 
 let tela = innerWidth
 if (tela <= 600) {
-    document.getElementById('logo').src = './assets/logo-mobile.svg'
+    document.getElementById('logo').src = './images/logo-mobile.svg'
 } else {
-    document.getElementById('logo').src = './assets/logo-desktop.svg'
+    document.getElementById('logo').src = './images/logo-desktop.svg'
 }
